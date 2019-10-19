129c5955dca3ebf5435a0ef1d8b467e57837c224
Daniel Moster <d.moster@icloud.com>
2019-10-19 18:55:26 -0400

    db troubleshoot 69

--- web/week05/prove05/get_user_devices.php
+++ web/week05/prove05/get_user_devices.php
@@ -22,11 +22,9 @@ $locationStr = "[ ";
 while ($location = $location_row->fetch(PDO::FETCH_ASSOC)) {
   $location_id = $location['location_id'];
   $location_name = $location['location_name'];
-  $locationStr += '{
-    "building": "' . $location_name . '",
-    "floors": [ ';
+  $locationStr += '{ "building": "' . $location_name . '", "floors": [ ';
     
-    echo $location_name;
+    echo $locationStr;
   $floor_row = $db->prepare("SELECT floor_id, floor_name FROM floors WHERE location_id=$location_id");
   $floor_row->execute();
 
