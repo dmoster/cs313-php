942f7debb57b385c3ee98b176b58eb3390d30c91
Daniel Moster <d.moster@icloud.com>
2019-10-19 17:47:36 -0400

    db troubleshoot 17

--- web/week05/prove05/get_device_json.js
+++ web/week05/prove05/get_device_json.js
@@ -9,6 +9,6 @@ function getDeviceJSON(username) {
     }
   };
 
-  xmlhttp.open("POST", "get_user_devices.php", true);
-  xmlhttp.send("username=" + username);
+  xmlhttp.open("GET", "get_user_devices.php?username=", true);
+  xmlhttp.send();
 }
--- web/week05/prove05/get_user_devices.php
+++ web/week05/prove05/get_user_devices.php
@@ -5,7 +5,7 @@ session_start();
 require "db_connect.php";
 $db = getDatabase();
 
-$username = $_POST['username'];
+$username = $_GET['username'];
 
 $user_row = $db->prepare("SELECT user_id FROM users WHERE username='$username' LIMIT 1");
 $user_row->execute();
