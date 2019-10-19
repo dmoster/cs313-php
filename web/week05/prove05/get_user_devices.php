4284f38bf1c0d3b3d933d35c72e0cb0a3b1768a7
Daniel Moster <d.moster@icloud.com>
2019-10-19 18:49:33 -0400

    db troubleshoot 63

--- web/week05/prove05/get_user_devices.php
+++ web/week05/prove05/get_user_devices.php
@@ -37,8 +37,8 @@ while ($location = $location_row->fetch(PDO::FETCH_ASSOC)) {
     $locationStr += '{ "name": "' . $floor_name . '",
         "devices": [ ';
   
-  //   $device_row = $db->prepare("SELECT device_id, device_description, device_notes, device_address, device_type, device_canFrame FROM devices WHERE floor_id=$floor_id");
-  //   $device_row->execute();
+    $device_row = $db->prepare("SELECT device_id, device_description, device_notes, device_address, device_type, device_canFrame FROM devices WHERE floor_id=$floor_id");
+    $device_row->execute();
 
   //   while ($device = $device_row->fetch(PDO::FETCH_ASSOC)) {
   //     $device_id = $device['device_id'];
