6d1185c7a510920287d4c9f9bc47a1da20dce9d0
Daniel Moster <d.moster@icloud.com>
2019-10-19 18:19:37 -0400

    db troubleshoot 39

--- web/week05/prove05/get_user_devices.php
+++ web/week05/prove05/get_user_devices.php
@@ -14,6 +14,8 @@ $user = $user_row->fetch(PDO::FETCH_ASSOC);
 
 $user_id = $user['user_id'];
 
+echo $user_id;
+
 $location_row = $db->prepare("SELECT location_id, location_name FROM locations WHERE user_id=2");
 $location_row->execute();
 
