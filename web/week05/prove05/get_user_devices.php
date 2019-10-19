3b401b6ee13fcfe5cdc6518f8860715b7f4ce9a2
Daniel Moster <d.moster@icloud.com>
2019-10-19 18:22:04 -0400

    db troubleshoot 41

--- web/week05/prove05/get_user_devices.php
+++ web/week05/prove05/get_user_devices.php
@@ -7,6 +7,8 @@ $db = getDatabase();
 
 $username = $_POST['username'];
 
+echo $username;
+
 $user_row = $db->prepare("SELECT user_id FROM users WHERE username='$username' LIMIT 1");
 $user_row->execute();
 
