ab5f56dca0cd87c725845c042ac07d8afabe993d
Daniel Moster <d.moster@icloud.com>
2019-10-19 19:22:16 -0400

    troubleshoot locations

--- web/week05/prove05/get_device_json.js
+++ web/week05/prove05/get_device_json.js
@@ -4,8 +4,8 @@ function getDeviceJSON(username) {
 
   xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      console.log(this.responseText);
       locations = JSON.parse(this.responseText);
+      console.log(locations);
     }
   };
 
