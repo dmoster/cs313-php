9cabad87b7659889bef412265b4a99cc47a48465
Daniel Moster <d.moster@icloud.com>
2019-10-19 18:43:48 -0400

    db troubleshoot 57

--- web/week05/prove05/get_user_devices.php
+++ web/week05/prove05/get_user_devices.php
@@ -20,52 +20,52 @@ $location_row->execute();
 $locationStr = "[ ";
 
 while ($location = $location_row->fetch(PDO::FETCH_ASSOC)) {
-  //   $location_id = $location['location_id'];
-  //   $location_name = $location['location_name'];
+  $location_id = $location['location_id'];
+  $location_name = $location['location_name'];
+
+  $locationStr += '{
+      "building": "' . $location_name . '",
+      "floors": [ ';
   
-  //   $locationStr += '{
-    //     "building": "' . $location_name . '",
-    //     "floors": [ ';
-    
-    //   $floor_row = $db->prepare("SELECT floor_id, floor_name FROM floors WHERE location_id=$location_id");
-    //   $floor_row->execute();
-    
-    //   while ($floor = $floor_row->fetch(PDO::FETCH_ASSOC)) {
-      //     $floor_id = $floor['floor_id'];
-      //     $floor_name = $floor['floor_name'];
-      
-      //     $locationStr += '{ "name": "' . $floor_name . '",
-        //       "devices": [ ';
-        
-        //     $device_row = $db->prepare("SELECT device_id, device_description, device_notes, device_address, device_type, device_canFrame FROM devices WHERE floor_id=$floor_id");
-        //     $device_row->execute();
-        
-        //     while ($device = $device_row->fetch(PDO::FETCH_ASSOC)) {
-          //       $device_id = $device['device_id'];
-          //       $device_description = $device['device_description'];
-          //       $device_notes = $device['device_notes'];
-          //       $device_address = $device['device_address'];
-          //       $device_type = $device['device_type'];
-          //       $device_canFrame = $device['device_canFrame'];
-          
-          //       $locationStr += '{
-            //         "address": "' . $device_address . '",
-            //         "label": "' . $device_description . '",
-            //         "notes": "' . $device_notes . '",
-            //         "type": "' . $device_type . '",
-            //         "canFrame": ' . $device_canFrame . '
-            //       }';
-            //     }
-            
-            //     $locationStr += '] },';
-            //   }
-            
-            //   $locationStr += '] },';
-            }
-            
-            $locationStr += " ]";
-            
-            echo $locationStr;
-            echo '{ "user_id": "' . $user_id . '" }';
-            
-            ?>
+  $floor_row = $db->prepare("SELECT floor_id, floor_name FROM floors WHERE location_id=$location_id");
+  $floor_row->execute();
+
+  while ($floor = $floor_row->fetch(PDO::FETCH_ASSOC)) {
+    $floor_id = $floor['floor_id'];
+    $floor_name = $floor['floor_name'];
+
+    $locationStr += '{ "name": "' . $floor_name . '",
+        "devices": [ ';
+  
+    $device_row = $db->prepare("SELECT device_id, device_description, device_notes, device_address, device_type, device_canFrame FROM devices WHERE floor_id=$floor_id");
+    $device_row->execute();
+
+    while ($device = $device_row->fetch(PDO::FETCH_ASSOC)) {
+      $device_id = $device['device_id'];
+      $device_description = $device['device_description'];
+      $device_notes = $device['device_notes'];
+      $device_address = $device['device_address'];
+      $device_type = $device['device_type'];
+      $device_canFrame = $device['device_canFrame'];
+
+      $locationStr += '{
+          "address": "' . $device_address . '",
+          "label": "' . $device_description . '",
+          "notes": "' . $device_notes . '",
+          "type": "' . $device_type . '",
+          "canFrame": ' . $device_canFrame . '
+        }';
+      }
+  
+      $locationStr += '] },';
+    }
+  
+    $locationStr += '] },';
+  }
+  
+  $locationStr += " ]";
+  
+  echo $locationStr;
+  echo '{ "user_id": "' . $user_id . '" }';
+  
+  ?>
