f67e13dbf44a12d78032012f222c56eb8d1ec2f3
dmoster <22249073+dmoster@users.noreply.github.com>
2019-10-25 17:07:21 -0400

    add new device form separate

---
+++ web/week05/prove05/add_devices.php
@@ -0,0 +1,88 @@
+<?php
+
+session_start();
+
+require "db_connect.php";
+$db = getDatabase();
+
+$username = $_SESSION['username'];
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["device_name"])) {
+    $devNameErr = "Please enter a device name.";
+  }
+  else {
+    $device_name = test_input($_POST["device_name"]);
+  }
+
+  if (!empty($_POST["device_description"])) {
+    $device_description = test_input($_POST["device_description"]);
+  }
+
+  if (empty($_POST["device_address"])) {
+    $devAddrErr = "Please enter a URL for the device.";
+  }
+  else {
+    $device_address = test_input($_POST["device_address"]);
+
+  }
+}
+
+
+function test_input($data) {
+$data = trim($data);
+$data = stripslashes($data);
+$data = htmlspecialchars($data);
+
+return $data;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <meta charset="UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+
+  <link rel="icon" href="images/copier.png" type="image/gif" sizes="16x16"/>
+  <link rel="stylesheet" href="pd.css"/>
+  <script src="https://kit.fontawesome.com/599e60a037.js" crossorigin="anonymous"></script>
+
+  <title>Add Devices</title>
+
+</head>
+<body id="device-adder">
+
+  <main>
+    <div class="card">
+      <h1>Add a Device</h1>
+
+      <form id="add-device" class="radius-tr" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
+        <input type="text" name="device_name" placeholder="Name" value="<?=$device_name;?>">
+        <span class="error"><?=$devNameErr;?></span>
+
+        <input type="text" name="device_description" placeholder="Description" value="<?=$device_description;?>">
+
+        <input type="url" name="device_address" placeholder="https://example.com" value="<?=$device_address;?>">
+        <span class="error"><?=$devAddrErr;?></span>
+
+        <select name="device_type">
+          <option value="Computer">Computer</option>
+          <option value="Printer">Printer</option>
+          <option value="Server">Server</option>
+          <option value="Phone">Phone</option>
+          <option value="Appliance">Appliance</option>
+          <option value="Other">Other</option>
+        </select>
+
+        <button class="btn" type="submit">Add</button>
+      </form>
+    </div>
+  </main>
+
+</body>
+</html>
--- web/week05/prove05/pd.php
+++ web/week05/prove05/pd.php
@@ -15,37 +15,6 @@ $user = $user_row->fetch(PDO::FETCH_ASSOC);
 $firstname = $user['firstname'];
 $lastname = $user['lastname'];
 
-
-if ($_SERVER["REQUEST_METHOD"] == "GET") {
-  if (empty($_POST["device_name"])) {
-    $devNameErr = "Please enter a device name.";
-  }
-  else {
-    $device_name = test_input($_POST["device_name"]);
-  }
-
-  if (!empty($_POST["device_description"])) {
-    $device_description = test_input($_POST["device_description"]);
-  }
-
-  if (empty($_POST["device_address"])) {
-    $devAddrErr = "Please enter a URL for the device.";
-  }
-  else {
-    $device_address = test_input($_POST["device_address"]);
-
-  }
-}
-
-
-function test_input($data) {
-$data = trim($data);
-$data = stripslashes($data);
-$data = htmlspecialchars($data);
-
-return $data;
-}
-
 ?>
 
 <!DOCTYPE html>
@@ -77,23 +46,11 @@ return $data;
       </form>
 
       <nav id="main-nav" class="radius-r"></nav>
-    
-      <div id="list-actions">
-        <form id="add-device" class="radius-tr" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="GET">
-          <span class="lead">Add a device</span>
-          <input type="text" name="device_name" placeholder="Name" value="<?=$device_name;?>">
-          <span class="error"><?=$devNameErr;?></span>
-          <input type="text" name="device_description" placeholder="Description" value="<?=$device_description;?>">
-          <input type="url" name="device_address" placeholder="https://example.com" value="<?=$device_address;?>">
-          <span class="error"><?=$devAddrErr;?></span>
-          <button class="btn" type="submit">Add</button>
-        </form>
   
-        <div id="layout_toggle">
-          <a href="#intro" class="inline-btn"><i class="fas fa-arrow-up"></i></a>
-          <button class="aside_btn" onclick="displayDeviceGrid(locations)"><i class="fas fa-border-all"></i></button>
-          <button class="aside_btn radius-br" onclick="displayDeviceTable(locations)"><i class="fas fa-list"></i></button>
-        </div>
+      <div id="layout_toggle">
+        <a href="#intro" class="inline-btn"><i class="fas fa-arrow-up"></i></a>
+        <button class="aside_btn" onclick="displayDeviceGrid(locations)"><i class="fas fa-border-all"></i></button>
+        <button class="aside_btn radius-r" onclick="displayDeviceTable(locations)"><i class="fas fa-list"></i></button>
       </div>
 
     </aside>
