afa3f6a7e200e90403855ed0e56175bf27dc48e8
dmoster <22249073+dmoster@users.noreply.github.com>
2019-10-25 12:46:15 -0400

    make AJAX post4

--- web/week05/prove05/get_user_devices.php
+++ web/week05/prove05/get_user_devices.php
@@ -4,7 +4,7 @@ require "db_connect.php";
 $db = getDatabase();
 
 $username =  $_POST['username'];
-
+echo $username;
 $user_row = $db->prepare("SELECT user_id FROM users WHERE username='$username' LIMIT 1");
 $user_row->execute();
 
--- web/week05/prove05/pd.js
+++ web/week05/prove05/pd.js
@@ -48,6 +48,5 @@ var deviceDisplay = document.getElementById('device-list');
 var username = document.getElementById('username').innerHTML;
 
 if (!deviceDisplay.innerHTML) {
-  console.log(username);
   getDeviceJSON(username);
 }
