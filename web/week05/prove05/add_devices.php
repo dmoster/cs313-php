844392598dd2148d5a1bd89248d27cc0d77d1919
Daniel Moster <d.moster@icloud.com>
2019-10-26 02:32:47 -0400

    add locations to add form30

--- web/week05/prove05/add_devices.php
+++ web/week05/prove05/add_devices.php
@@ -8,11 +8,11 @@ $db = getDatabase();
 $user_id;
 $user;
 
-$username = $_POST['username'];
+$username = $_POST['add_to'];
 $_SESSION['username'] = $username;
 
 try {
-  $user_row = $db->prepare("SELECT * FROM users WHERE username='$username' LIMIT 1");
+  $user_row = $db->prepare("SELECT user_id FROM users WHERE username='$username' LIMIT 1");
   $user_row->execute();
 
   $user = $user_row->fetch(PDO::FETCH_ASSOC);
