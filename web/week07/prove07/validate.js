da4a13521e47e4f3bc49d51ea0462bb9ac39920f
Daniel Moster <d.moster@icloud.com>
2019-11-02 13:13:28 -0400

    fix validate.js0

--- web/week07/prove07/validate.js
+++ web/week07/prove07/validate.js
@@ -9,7 +9,7 @@ function comparePw() {
 
   if (pw1 === pw2) {
     pwField2.style.color = 'mediumseagreen';
-    signUpBtn.style.display = '';
+    signUpBtn.style.display = 'block';
   }
   else {
     pwField2.style.color = 'tomato';
